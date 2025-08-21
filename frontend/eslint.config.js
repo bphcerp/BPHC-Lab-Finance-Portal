30ea3164728ddc653e161b6745db6a719e34e2a3
catfo435 <pranavlsgreat210120@gmail.com>
2025-08-21 20:02:58 +0530

    ci: added type checks on commit to main

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -19,8 +19,8 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      "typescript-eslint/no-explicit-any": false,
-      "react-hooks/exhaustive-deps": false,
+      "@typescript-eslint/no-explicit-any": "off",
+      "react-hooks/exhaustive-deps": "off",
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
